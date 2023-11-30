931c37fb212c7f3987a2c06705cd3372e3e7777a
Pariskar <pariskar2001@gmail.com>
2023-11-30 23:42:43 +0545

    try

--- src/components/FaceRecognizer/FaceRecognizer.js
+++ src/components/FaceRecognizer/FaceRecognizer.js
@@ -8,7 +8,7 @@ const FaceRecognizer = ({imageurl, boxes}) => {
         <img src={imageurl} id="inputimage" alt="" width='500px' height='auto'/> 
         {/* esari auta chijlai auto diyesi dami hunxa, width anusar image nafatni gari height adjust garxa  */}
         {boxes.map((item)=>{
-            <div className="bounding-box" style={{left: item.leftCol, top: item.topRow, right: item.rightCol, bottom: item.bottomRow}}></div>
+            return(<div className="bounding-box" style={{left: item.leftCol, top: item.topRow, right: item.rightCol, bottom: item.bottomRow}}></div>)
         })}
         {/* relative garaepar laskarai hunthyo, image paxi div hunthyo
         absolute garayesi, comes out of the flow , ani esko lagi space ni xuttako hunna, wrt parent, positions dini aba , overlap gardinxa aru kuralai  */}
